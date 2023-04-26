03b30cb22004a86a128f64564588cb0d6c1e5928
Rakesh Bhardwaj <Rb610883@gmail.com>
2023-04-26 20:57:41 +0530

    Starting registration using the algolia.

--- app/src/main/java/rakesh/app/BusTimingApp/MainActivity.java
+++ app/src/main/java/rakesh/app/BusTimingApp/MainActivity.java
@@ -1,14 +1,70 @@
 package rakesh.app.BusTimingApp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
+    EditText etStoppage, etDestination;
+    ImageView btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawableLayout);
+        navigationView = findViewById(R.id.sideBar);
+        toolbar = findViewById(R.id.toolBar);
+
+        etStoppage = findViewById(R.id.etStoppage);
+        etDestination = findViewById(R.id.etStoppage);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.OpenSidebar,R.string.CloseSidebar);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+
+
+                    case R.id.menu_home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.menu_registerBusDetails:
+                        startActivity(new Intent(getApplicationContext(),RegisterBusDetails.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/rakesh/app/BusTimingApp/RegisterBusDetails.java
@@ -0,0 +1,14 @@
+package rakesh.app.BusTimingApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterBusDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_bus_details);
+    }
+}
